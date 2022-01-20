0a69dd30219cc3fe017483cafe815f2b6f557c05
KarloFab <fabijanickarlo1@gmail.com>
2022-01-20 22:32:01 +0100

    Added new methods for num or calculate pressed

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
+ 
+    
+    @IBAction func numberButtonPressed(_ sender: Any) {
+    }
 }
 
